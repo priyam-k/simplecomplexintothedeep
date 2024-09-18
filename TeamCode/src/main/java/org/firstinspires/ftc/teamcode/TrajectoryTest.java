c9033a1a49bd92d7e61c982f897e49025c3f3e58
Chelsea Wang <chelsea.wang86@gmail.com>
2024-09-18 17:03:44 -0400

    placer code for nemo

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TrajectoryTest.java
@@ -0,0 +1,70 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.acmerobotics.roadrunner.Pose2d;
+import com.acmerobotics.roadrunner.Trajectory;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+public class TrajectoryTest extends LinearOpMode {
+    @Override
+    public void runOpMode() throws InterruptedException {
+        MecanumDrive drive = new MecanumDrive(hardwareMap, new Pose2d(0, 60, Math.toRadians(90)));
+
+        Trajectory myTrajectory = drive.actionBuilder(drive.pose)
+                .lineToYSplineHeading(33, Math.toRadians(0))
+                .waitSeconds(2)
+                .setTangent(Math.toRadians(90))
+                .lineToY(48)
+                .setTangent(Math.toRadians(0))
+                .lineToX(32)
+                .strafeTo(new Vector2d(44.5, 30))
+                .turn(Math.toRadians(180))
+                .lineToX(47.5)
+                .waitSeconds(3)
+                .build();
+        waitForStart();
+
+        if(isStopRequested()) return;
+
+        drive.followTrajectory(myTrajectory);
+    }
+
+    //                        .lineToY(24)
+//                        .waitSeconds(2.0)
+//                        .lineToY(40)
+//                        .turn(Math.toRadians(-90))
+//                        .lineToX(-48)
+//                        .turn(Math.toRadians(90))
+//                        .lineToY(33)
+//                        .waitSeconds(1.0)
+//                        .lineToY(40)
+//                        .turn(Math.toRadians(90))
+//                        .lineToX(55)
+//                        .turn(Math.toRadians(-90))
+//                        .lineToY(63)
+//                        .waitSeconds(1.0)
+//                        .lineToY(40)
+//                        .turn(Math.toRadians(-90))
+//                        .lineToX(-58)
+//                        .turn(Math.toRadians(90))
+//                        .lineToY(33)
+//                        .waitSeconds(1.0)
+//                        .lineToY(40)
+//                        .turn(Math.toRadians(90))
+//                        .lineToX(55)
+//                        .turn(Math.toRadians(-90))
+//                        .lineToY(63)
+//                        .waitSeconds(1.0)
+//                        .lineToY(40)
+//                        .turn(Math.toRadians(-90))
+//                        .lineToX(-68)
+//                        .turn(Math.toRadians(90))
+//                        .lineToY(33)
+//                        .waitSeconds(1.0)
+//                        .lineToY(40)
+//                        .turn(Math.toRadians(90))
+//                        .lineToX(55)
+//                        .turn(Math.toRadians(-90))
+//                        .lineToY(63)
+
+    }
+}
