10a1ea5ddcbf76cc8bab0b6cec03213565e8476e
Chelsea Wang <chelsea.wang86@gmail.com>
2024-09-22 15:02:42 -0400

    finished two paths and put them in auto package

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RoadRunner/autos/BlueAllianceObsZone.java
@@ -0,0 +1,48 @@
+package org.firstinspires.ftc.teamcode.RoadRunner.autos;
+
+import com.acmerobotics.roadrunner.Action;
+import com.acmerobotics.roadrunner.Pose2d;
+import com.acmerobotics.roadrunner.SequentialAction;
+import com.acmerobotics.roadrunner.ftc.Actions;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import org.firstinspires.ftc.teamcode.RoadRunner.MecanumDrive;
+
+@Autonomous (name = "BlueAllianceObsZone")
+public class BlueAllianceObsZone extends LinearOpMode {
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        MecanumDrive drive = new MecanumDrive(hardwareMap, new Pose2d(-70, 0, Math.toRadians(0)));
+
+        Action myTrajectory = drive.actionBuilder(drive.pose)
+                .splineToLinearHeading(new Pose2d(-35, 52, Math.toRadians(-180)), Math.toRadians(180))
+                .turn(Math.toRadians(25))
+                .lineToY(42)
+                .turn(Math.toRadians(65))
+                .waitSeconds(1)
+                .turn(Math.toRadians(10))
+                .lineToY(60)
+                .turn(Math.toRadians(25))
+                .waitSeconds(1)
+                .turn(Math.toRadians(-10))
+                .lineToY(40)
+                .turn(Math.toRadians(-15))
+                .waitSeconds(1)
+                .lineToY(60)
+                .turn(Math.toRadians(30))
+                .waitSeconds(1)
+                .turn(Math.toRadians(-60))
+                .lineToY(40)
+                .waitSeconds(1)
+                .turn(Math.toRadians(25))
+                .lineToY(60)
+                .build();
+        waitForStart();
+
+        if(isStopRequested()) return;
+
+        Actions.runBlocking(new SequentialAction(myTrajectory));
+    }
+}
