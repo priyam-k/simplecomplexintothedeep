149a23e823815cf4bf3085ca53e2269934e54bc6
adi <awesomeadi2007@gmail.com>
2024-10-08 20:13:04 -0400

    More comprehensive servo tester

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ServoAndPortTester.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ServoAndPortTester.java
@@ -10,18 +10,18 @@ import com.qualcomm.robotcore.hardware.Servo;
 @TeleOp(name="ServoAndPortTest")
 public class ServoAndPortTester extends LinearOpMode {
     private MultipleTelemetry tele;
-    private Servo testServo0;
-    private Servo testServo1;
-    private Servo testServo2;
-    private Servo testServo3;
-    private Servo testServo4;
-    private Servo testServo5;
-    private Servo testServo6;
-    private Servo testServo7;
-    private Servo testServo8;
-    private Servo testServo9;
-    private Servo testServo10;
-    private Servo testServo11;
+    private Servo ControlHub0;
+    private Servo ControlHub1;
+    private Servo ControlHub2;
+    private Servo ControlHub3;
+    private Servo ControlHub4;
+    private Servo ControlHub5;
+    private Servo ExpansionHub0;
+    private Servo ExpansionHub1;
+    private Servo ExpansionHub2;
+    private Servo ExpansionHub3;
+    private Servo ExpansionHub4;
+    private Servo ExpansionHub5;
 
     public static double Servo0Pos = 0;
     public static double Servo1Pos = 0;
@@ -37,70 +37,67 @@ public class ServoAndPortTester extends LinearOpMode {
     public static double Servo10Pos = 0;
     public static double Servo11Pos = 0;
 
-
-
     @Override
     public void runOpMode() {
         FtcDashboard dashboard = FtcDashboard.getInstance();
         tele = new MultipleTelemetry(telemetry, dashboard.getTelemetry());
 
-        // Initialize the servo
+        // Initialize the servos for the Control Hub
+        ControlHub0 = hardwareMap.get(Servo.class, "Servo0");
+        ControlHub1 = hardwareMap.get(Servo.class, "Servo1");
+        ControlHub2 = hardwareMap.get(Servo.class, "Servo2");
+        ControlHub3 = hardwareMap.get(Servo.class, "Servo3");
+        ControlHub4 = hardwareMap.get(Servo.class, "Servo4");
+        ControlHub5 = hardwareMap.get(Servo.class, "Servo5");
 
-        // Control Hub
-        testServo0 = hardwareMap.get(Servo.class, "Servo0");
-        testServo1 = hardwareMap.get(Servo.class, "Servo1");
-        testServo2 = hardwareMap.get(Servo.class, "Servo2");
-        testServo3 = hardwareMap.get(Servo.class, "Servo3");
-        testServo4 = hardwareMap.get(Servo.class, "Servo4");
-        testServo5 = hardwareMap.get(Servo.class, "Servo5");
-
-        // Expansion Hub
-        testServo6  = hardwareMap.get(Servo.class, "Servo6");
-        testServo7  = hardwareMap.get(Servo.class, "Servo7");
-        testServo8  = hardwareMap.get(Servo.class, "Servo8");
-        testServo9  = hardwareMap.get(Servo.class, "Servo1");
-        testServo10 = hardwareMap.get(Servo.class, "Servo10");
-        testServo11 = hardwareMap.get(Servo.class, "Servo11");
-
+        // Initialize the servos for the Expansion Hub
+        ExpansionHub0  = hardwareMap.get(Servo.class, "Servo6");
+        ExpansionHub1  = hardwareMap.get(Servo.class, "Servo7");
+        ExpansionHub2  = hardwareMap.get(Servo.class, "Servo8");
+        ExpansionHub3  = hardwareMap.get(Servo.class, "Servo9");
+        ExpansionHub4 = hardwareMap.get(Servo.class, "Servo10");
+        ExpansionHub5 = hardwareMap.get(Servo.class, "Servo11");
 
         waitForStart();
 
         while (opModeIsActive()) {
-            // Servo 0 Pos vals
-            testServo0.setPosition(Servo0Pos);
-            telemetry.addData("Servo 0 Position", Servo0Pos);
-            // Servo 1 Pos vals
-            testServo1.setPosition(Servo1Pos);
-            telemetry.addData("Servo 1 Position", Servo1Pos);
-            // Servo 2 Pos vals
-            testServo2.setPosition(claw);
-            telemetry.addData("Servo 2 Position", claw);
-            // Servo 3 Pos vals
-            testServo3.setPosition(swingArm);
-            testServo5.setPosition(swingArm);
-            telemetry.addData("Servo 3 Position", swingArm);
-            telemetry.addData("Servo 5 position", rightArm);
-            // Servo 4 Pos vals
-            testServo4.setPosition(clawTurret);
-            telemetry.addData("Servo 4 Position", clawTurret);
-            // Servo 6 Pos vals
-            testServo6.setPosition(Servo6Pos);
-            telemetry.addData("Servo 6 Position", Servo6Pos);
-            // Servo 7 Pos vals
-            testServo7.setPosition(turretPos);
-            telemetry.addData("Servo 7 Position", turretPos);
-            // Servo 8 Pos vals
-            testServo8.setPosition(Servo8Pos);
-            telemetry.addData("Servo 8 Position", Servo8Pos);
-            // Servo 9 Pos vals
-            testServo9.setPosition(Servo9Pos);
-            telemetry.addData("Servo 9 Position", Servo9Pos);
-            // Servo 10 Pos vals
-            testServo10.setPosition(Servo10Pos);
-            telemetry.addData("Servo 10 Position", Servo10Pos);
-            // Servo 11 Pos vals
-            testServo11.setPosition(Servo11Pos);
-            telemetry.addData("Servo 11 Position", Servo11Pos);
+            // Set and display positions for Control Hub servos
+            ControlHub0.setPosition(Servo0Pos);
+            telemetry.addData("ControlHub0 Position", Servo0Pos);
+
+            ControlHub1.setPosition(Servo1Pos);
+            telemetry.addData("ControlHub1 Position", Servo1Pos);
+
+            ControlHub2.setPosition(claw);
+            telemetry.addData("ControlHub2 Position", claw);
+
+            ControlHub3.setPosition(swingArm);
+            telemetry.addData("ControlHub3 Position", swingArm);
+
+            ControlHub4.setPosition(clawTurret);
+            telemetry.addData("ControlHub4 Position", clawTurret);
+
+            ControlHub5.setPosition(rightArm);
+            telemetry.addData("ControlHub5 Position", rightArm);
+
+            // Set and display positions for Expansion Hub servos
+            ExpansionHub0.setPosition(Servo6Pos);
+            telemetry.addData("ExpansionHub0 Position", Servo6Pos);
+
+            ExpansionHub1.setPosition(turretPos);
+            telemetry.addData("ExpansionHub1 Position", turretPos);
+
+            ExpansionHub2.setPosition(Servo8Pos);
+            telemetry.addData("ExpansionHub2 Position", Servo8Pos);
+
+            ExpansionHub3.setPosition(Servo9Pos);
+            telemetry.addData("ExpansionHub3 Position", Servo9Pos);
+
+            ExpansionHub4.setPosition(Servo10Pos);
+            telemetry.addData("ExpansionHub4 Position", Servo10Pos);
+
+            ExpansionHub5.setPosition(Servo11Pos);
+            telemetry.addData("ExpansionHub5 Position", Servo11Pos);
 
             telemetry.update();
         }
