6b1bf80ebc125a1b432b063de7cf812ba02148fa
amithi <amithi.munagala@gmail.com>
2024-10-26 10:53:59 -0400

    Parking autons finished

---
+++ MeepMeep/src/main/java/com/example/meepmeep/BlueAllainceParkNetZone.java
@@ -0,0 +1,30 @@
+package com.example.meepmeep;
+
+import com.acmerobotics.roadrunner.Pose2d;
+import com.noahbres.meepmeep.MeepMeep;
+import com.noahbres.meepmeep.roadrunner.DefaultBotBuilder;
+import com.noahbres.meepmeep.roadrunner.entity.RoadRunnerBotEntity;
+
+public class BlueAllainceParkNetZone {
+    public static void main(String[] args) {
+        MeepMeep meepMeep = new MeepMeep(750);
+
+        RoadRunnerBotEntity myBot = new DefaultBotBuilder(meepMeep)
+                // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
+                .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 15)
+                .build();
+
+        myBot.runAction(myBot.getDrive().actionBuilder(new Pose2d(0, 31, Math.toRadians(90)))
+                .splineToLinearHeading(new Pose2d(37, 33, Math.toRadians(90)), Math.toRadians(270))
+                .splineToLinearHeading(new Pose2d(26, 9, Math.toRadians(90)), Math.toRadians(180))
+                .build());
+
+
+
+        meepMeep.setBackground(MeepMeep.Background.FIELD_INTO_THE_DEEP_JUICE_DARK)
+                .setDarkMode(true)
+                .setBackgroundAlpha(0.95f)
+                .addEntity(myBot)
+                .start();
+    }
+}
